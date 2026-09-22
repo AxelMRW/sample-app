ff6289e20267f02889cd97784cbb076c8b8a010f
AxelMRW <20233tn039@utez.edu.mx>
2026-09-22 07:16:02 +0000

    Changed port from 8080 to 5050.

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm -rf tempdir
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -8,15 +8,19 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "FROM python:3.8-slim" > tempdir/Dockerfile
+echo "RUN pip install --progress-bar off flask==2.0.1 werkzeug==2.0.1" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "WORKDIR /home/myapp/" >> tempdir/Dockerfile
+echo "CMD [\"python3\", \"sample_app.py\"]" >> tempdir/Dockerfile
 
 cd tempdir
+docker stop samplerunning 2>/dev/null || true
+docker rm samplerunning 2>/dev/null || true
 docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
-docker ps -a 
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+sleep 3
+docker ps
